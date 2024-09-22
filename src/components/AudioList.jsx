905ba6be0bc415d4271860c372c2c1e1553a454a
cesardmn <cesardmn@gmail.com>
2024-09-22 10:07:46 -0300

    add sort AudioList

--- src/components/AudioList.jsx
+++ src/components/AudioList.jsx
@@ -6,27 +6,30 @@ export default function AudioList() {
   const { playlist } = usePlaylist()
   const { setAudio, audio } = useAudio()
 
-  const habdleSelect = (music) => {
+  const handleSelect = (music) => {
     setAudio(music)
   }
 
+
   return (
-    <ul className={`${styles.ul} ${styles.noSrcollBar}`}>
-      {playlist.map((music) => {
-        const isSelected = music === audio
-        return (
-          <li
-            key={music.name}
-            className={`${styles.li} ${isSelected && styles.selected}`}
-            onClick={() => {
-              habdleSelect(music)
-            }}
-          >
-            <span>{music.name}</span>
-            <span className={styles.duration}>{music.duration}</span>
-          </li>
-        )
-      })}
+    <ul className={`${styles.ul} ${styles.noScrollBar}`}>
+      {
+        playlist
+          .sort((a, b) => a.name.localeCompare(b.name))
+          .map((music, index) => {
+            const isSelected = music === audio;
+            return (
+              <li
+                key={`${music.name}-${index}`}
+                className={`${styles.li} ${isSelected ? styles.selected : ''}`}
+                onClick={() => handleSelect(music)}
+              >
+                <span>{music.name}</span>
+                <span className={styles.duration}>{music.duration}</span>
+              </li>
+            );
+          })
+      }
     </ul>
   )
 }
